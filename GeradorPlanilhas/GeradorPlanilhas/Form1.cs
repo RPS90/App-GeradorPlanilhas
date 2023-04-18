43e435985d45923169be289cb8d69df7c789f7a6
Rafael Pereira da Silva <horohoro@live.com>
2023-04-17 22:29:16 -0700

    Alteração de framework

--- GeradorPlanilhas/GeradorPlanilhas/Form1.cs
+++ GeradorPlanilhas/GeradorPlanilhas/Form1.cs
@@ -16,19 +16,19 @@ namespace GeradorPlanilhas
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
 
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
 
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
 
-
-
-
-
-
-
-
-
-
+        }
     }
 }
